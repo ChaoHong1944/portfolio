2bbde7f1165ae3431d160fc1981e0898f0f2290f
Chao Hong <91182144+ChaoHong1944@users.noreply.github.com>
2025-06-30 01:26:50 -0400

    updated portfolio

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -6,11 +6,11 @@ const Navbar = () => {
   return (
     <>
         <nav className='w-full fixed px-5 lg:px-8 xl:px-[8%] py-4 flex items-center justify-between z-50'>
-            <a href= "">
-                <Image src={assets.logo} className='w-28 alt="" cursor-pointer mr-14'/>
+            <a href="#top">
+                <Image src={assets.logo} className='w-28 cursor-pointer mr-14' alt=""/>
             </a>
 
-            <ul>
+            <ul className='hidden md:flex items-center gap-6 lg:gap-8 xl:gap-10 rounded-full px-12 py-3'>
                 <li><a href= "#top"> Home </a></li>
                 <li><a href= "#about"> About Me </a></li>
                 <li><a href= "#services"> Services </a></li>
